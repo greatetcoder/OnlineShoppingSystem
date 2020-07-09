5fb243f5dd4e95900fdaad2792d64fc8e158e032
greatetcoder <2601797515@qq.com>
2020-07-09 21:27:19 +0800

    创建类，实现商品名称、价格和数量的传递

---
+++ src/oss/GoodsSingle.java
@@ -0,0 +1,12 @@
+package oss;
+public class GoodsSingle {
+    private String name;
+    private float price;
+    private int num;
+    public String getName(){return name;}
+    public void setName(String name){this.name=name;}
+    public int getNum(){return num;}
+    public void setNum(int num){this.num=num;}
+    public float getPrice(){return price;}
+    public void setPrice(float price){this.price=price;}
+}
